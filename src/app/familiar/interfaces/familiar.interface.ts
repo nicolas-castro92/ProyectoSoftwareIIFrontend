997e1e77f69018f508bb0614e6606f0b7e1b636e
Nicolás Castro Giraldo <Nicolas.castro9219@gmail.com>
2023-06-08 19:50:38 -0500

    Add interface familiar

---
+++ src/app/familiar/interfaces/familiar.interface.ts
@@ -0,0 +1,34 @@
+export interface ListFamiliar {
+    name?:                string;
+    last_name?:           string;
+    identification_card?: string;
+    age?:                 number;
+    phone?:               string;
+    email?:               string;
+    address?:             string;
+    alternate_phone?:     string;
+}
+
+export interface CreateFamiliar {
+    name?:                string;
+    last_name?:           string;
+    identification_card?: string;
+    age?:                 number;
+    phone?:               string;
+    email?:               string;
+    address?:             string;
+    alternate_phone?:     string;
+
+}
+
+export interface EditFamiliar {
+    name?:                string;
+    last_name?:           string;
+    identification_card?: string;
+    age?:                 number;
+    phone?:               string;
+    email?:               string;
+    address?:             string;
+    alternate_phone?:     string;
+}
+
